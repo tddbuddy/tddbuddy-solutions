4e147ef094602a529ba40f9ca18ddbc79d049612
Travis Frisinger <tmfrisinger@gmail.com>
2023-04-21 11:22:37 -0600

    added balanced brackets in c#

---
+++ c#/Balanced-Brackets/BalancedBrackets/BalancedBrackets.Tests/BalancedBrackets.Tests.cs
@@ -0,0 +1,144 @@
+﻿using BalancedBracketsKata;
+
+namespace BalancedBracketsKataTests
+{
+
+    /*
+     * Partitions and Boundaries to be tested
+     * 
+     * Empty String
+     * Balanced Brackets
+     *  Simple []
+     *  Multiple [][]
+     *  Nested [[]][
+     *  Complex Nested [[[][]]]
+     * Unbalanced Brackets
+     *  Simple ][
+     *  Mulitiple ][][
+     *  Balanced and unbalanced mix [][]][
+     *  Nested unbalanced [[]][[[]
+     *  
+     */
+
+    [TestFixture]
+    public class BalancedBracketsTests
+    {
+        [Test]
+        public void TestEmptyString()
+        {
+            // Arrange
+            string input = "";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestSimpleBalancedBrackets()
+        {
+            // Arrange
+            string input = "[]";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestMultipleBalancedPairs()
+        {
+            // Arrange
+            string input = "[][]";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestNestedBalancedPairs()
+        {
+            // Arrange
+            string input = "[[]]";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestComplexNestedBalancedPairs()
+        {
+            // Arrange
+            string input = "[[[][]]]";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestSimpleUnbalancedBrackets()
+        {
+            // Arrange
+            string input = "][";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TestMultipleUnmatchedClosingBrackets()
+        {
+            // Arrange
+            string input = "][][";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TestBalancedPairsFollowedByUnmatchedClosingBrackets()
+        {
+            // Arrange
+            string input = "[][]][";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TestNestedUnbalancedBrackets()
+        {
+            // Arrange
+            string input = "[[]][[[]";
+
+            // Act
+            bool result = new BalancedBrackets().IsBalanced(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+        
+    }
+}
---
+++ c#/Balanced-Brackets/BalancedBrackets/BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,27 @@
+﻿namespace BalancedBracketsKata
+{
+    public class BalancedBrackets
+    {
+        public bool IsBalanced(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
