74497f05a41564f926f47641323981d7f9e87c34
Travis Frisinger <tmfrisinger@gmail.com>
2023-12-13 20:53:48 -0700

    added C# ip validator

---
+++ ip-validator/C#/IpValidator.Tests/IpValidatorTests.cs
@@ -0,0 +1,92 @@
+﻿using Validator;
+
+namespace IpValidationKata.Tests
+{
+    [TestFixture]
+    public class IpValidatorTests
+    {
+        private IpValidator CreateValidator()
+        {
+            return new IpValidator();
+        }
+
+        [TestCase("1.1.1.1", true)]
+        [TestCase("192.168.1.1", true)]
+        [TestCase("10.0.0.1", true)]
+        [TestCase("127.0.0.1", true)]
+        public void ValidateIpv4Address_BasicFormatTests(string ipAddress, bool expectedResult)
+        {
+            // Arrange
+            var validator = CreateValidator();
+
+            // Act
+            var result = validator.ValidateIpv4Address(ipAddress);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("256.1.1.1", false)]
+        [TestCase("192.300.1.1", false)]
+        [TestCase("10.0.0.256", false)]
+        public void ValidateIpv4Address_RangeTests(string ipAddress, bool expectedResult)
+        {
+            // Arrange
+            var validator = CreateValidator();
+
+            // Act
+            var result = validator.ValidateIpv4Address(ipAddress);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("01.1.1.1", false)]
+        [TestCase("192.168.01.1", false)]
+        [TestCase("10.0.0.01", false)]
+        public void ValidateIpv4Address_LeadingZeroTests(string ipAddress, bool expectedResult)
+        {
+            // Arrange
+            var validator = CreateValidator();
+
+            // Act
+            var result = validator.ValidateIpv4Address(ipAddress);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("192.168.1.0", false)]
+        [TestCase("0.0.0.0", false)]
+        [TestCase("255.255.255.255", false)]
+        public void ValidateIpv4Address_NetworkAndBroadcastAddressTests(string ipAddress, bool expectedResult)
+        {
+            // Arrange
+            var validator = CreateValidator();
+
+            // Act
+            var result = validator.ValidateIpv4Address(ipAddress);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase(" ", false)]
+        [TestCase("not.an.ip", false)]
+        [TestCase("1234.1234.1234.1234", false)]
+        [TestCase("....", false)]
+        public void ValidateIpv4Address_EdgeCaseTests(string ipAddress, bool expectedResult)
+        {
+            // Arrange
+            var validator = CreateValidator();
+
+            // Act
+            var result = validator.ValidateIpv4Address(ipAddress);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ ip-validator/C#/IpValidator/IpValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Validator
+{
+    public class IpValidator
+    {
+        public bool ValidateIpv4Address(string ipAddress)
+        {
+            if (string.IsNullOrWhiteSpace(ipAddress))
+                return false;
+
+            var parts = ipAddress.Split('.');
+            if (parts.Length != 4)
+                return false;
+
+            foreach (var part in parts)
+            {
+                if (!byte.TryParse(part, out byte bytePart))
+                    return false;
+
+                if (part.Length > 1 && part.StartsWith("0"))
+                    return false;
+            }
+
+            // Check if the last part is "0" or "255"
+            return !(parts[3] == "0" || parts[3] == "255");
+        }
+    }
+}
