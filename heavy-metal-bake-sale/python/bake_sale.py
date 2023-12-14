421e6d0c578cb4bcc2c7534712457b7f2c59c449
Travis Frisinger <tmfrisinger@gmail.com>
2023-12-13 18:44:26 -0700

    correct version of python bake sale

--- heavy-metal-bake-sale/python/bake_sale.py
+++ heavy-metal-bake-sale/python/bake_sale.py
@@ -1,29 +1,74 @@
+# Assuming that Python 3.6 or above is being used
+
 class BakeSaleItem:
-    def __init__(self, name, price, quantity):
-        self.name = name
+    def __init__(self, purchase_code, name, price, quantity):
+        self.purchase_code = purchase_code
         self.price = price
         self.quantity = quantity
+        self.name = name
 
-class BakeSale:
+
+class BakeSaleInventory:
     def __init__(self):
         self.items = {
-            "B": BakeSaleItem("Brownie", 0.75, 48),
-            "M": BakeSaleItem("Muffin", 1.00, 36),
-            "C": BakeSaleItem("Cake Pop", 1.35, 24),
-            "W": BakeSaleItem("Water", 1.50, 30)
+            "B": BakeSaleItem("B", "Brownie", 0.75, 48),
+            "M": BakeSaleItem("M", "Muffin", 1.00, 36),
+            "C": BakeSaleItem("C", "Cake Pop", 1.35, 24),
+            "W": BakeSaleItem("W", "Water", 1.50, 30),
         }
 
-    def process_order(self, order_codes, amount_paid):
-        total_cost = 0
-        for code in order_codes.split(','):
-            item = self.items.get(code)
-            if item and item.quantity > 0:
-                total_cost += item.price
-                item.quantity -= 1
-            else:
-                return f"{item.name if item else 'Item'} is out of stock" if item else "Invalid item code"
+    def get_quantity(self, purchase_code):
+        return self.items[purchase_code].quantity
 
-        if total_cost > amount_paid:
-            return "Not enough money"
-        return f"Change: ${amount_paid - total_cost:.2f}"
+    def get_price(self, purchase_code):
+        return self.items[purchase_code].price
+
+    def get_name(self, purchase_code):
+        return self.items[purchase_code].name
+
+    def update_stock(self, purchase_code, quantity):
+        self.items[purchase_code].quantity += quantity
+
+
+class IInputBuffer:
+    def read_purchase_input(self):
+        raise NotImplementedError
+
+    def read_amount_paid(self):
+        raise NotImplementedError
+
+
+class IOutputBuffer:
+    def write_message(self, message):
+        raise NotImplementedError
+
+
+class BakeSalePos:
+    def __init__(self, inventory, input_buffer, output_buffer):
+        self.inventory = inventory
+        self.input_buffer = input_buffer
+        self.output_buffer = output_buffer
+
+    def process_order(self):
+        order = self.input_buffer.read_purchase_input()
+        items = order.split(',')
+
+        total = 0
+
+        # Calculate total and check stock
+        for item in items:
+            if self.inventory.get_quantity(item) <= 0:
+                self.output_buffer.write_message(f"{self.inventory.get_name(item)} is out of stock")
+                return
+
+            total += self.inventory.get_price(item)
+            self.inventory.update_stock(item, 1)
+
+        amount_paid = self.input_buffer.read_amount_paid()
+
+        if amount_paid < total:
+            self.output_buffer.write_message("Not enough money")
+        else:
+            change = amount_paid - total
+            self.output_buffer.write_message(f"Change due: ${change:.2f}")
 
--- heavy-metal-bake-sale/python/test_bake_sale.py
+++ heavy-metal-bake-sale/python/test_bake_sale.py
@@ -1,26 +1,74 @@
 import pytest
-from bake_sale import BakeSale  # Replace with your actual module and class names
+from unittest.mock import Mock, create_autospec
+from bake_sale import BakeSalePos, BakeSaleInventory, IInputBuffer, IOutputBuffer
 
-def test_total_calculation():
-    bake_sale = BakeSale()
-    assert bake_sale.process_order("B,C,W", 4.00) == "Change: $0.40"
+# A test builder in Python isn't necessary in the same way because Python's
+# dynamic nature allows us to easily swap out components.
+# However, we can still use a setup function or fixtures to achieve similar setup.
 
-def test_exact_amount():
-    bake_sale = BakeSale()
-    assert bake_sale.process_order("B", 0.75) == "Change: $0.00"
+@pytest.fixture
+def bake_sale_inventory():
+    inventory = BakeSaleInventory()
+    # Additional setup if necessary
+    return inventory
 
-def test_insufficient_payment():
-    bake_sale = BakeSale()
-    assert bake_sale.process_order("C,M", 2.00) == "Not enough money"
+@pytest.fixture
+def input_buffer():
+    return create_autospec(IInputBuffer)
 
-def test_out_of_stock():
-    bake_sale = BakeSale()
-    # Simulate purchasing all water bottles
-    for _ in range(30):
-        bake_sale.process_order("W", 1.50)
-    assert bake_sale.process_order("W", 1.50) == "Water is out of stock"
+@pytest.fixture
+def output_buffer():
+    return create_autospec(IOutputBuffer)
 
-def test_invalid_code():
-    bake_sale = BakeSale()
-    assert bake_sale.process_order("Z", 1.00) == "Invalid item code"
+def test_in_stock_purchase(bake_sale_inventory, input_buffer, output_buffer):
+    # Arrange
+    input_buffer.read_purchase_input.return_value = "B"
+    input_buffer.read_amount_paid.return_value = 1.00
+
+    bake_sale = BakeSalePos(bake_sale_inventory, input_buffer, output_buffer)
+
+    # Act
+    bake_sale.process_order()
+
+    # Assert
+    output_buffer.write_message.assert_called_once_with("Change due: $0.25")
+
+def test_out_of_stock_purchase(bake_sale_inventory, input_buffer, output_buffer):
+    # Arrange
+    bake_sale_inventory.update_stock("W", -bake_sale_inventory.get_quantity("W"))  # Set Water stock to 0
+    input_buffer.read_purchase_input.return_value = "W"
+
+    bake_sale = BakeSalePos(bake_sale_inventory, input_buffer, output_buffer)
+
+    # Act
+    bake_sale.process_order()
+
+    # Assert
+    output_buffer.write_message.assert_called_once_with("Water is out of stock")
+
+def test_not_enough_money(bake_sale_inventory, input_buffer, output_buffer):
+    # Arrange
+    input_buffer.read_purchase_input.return_value = "C"
+    input_buffer.read_amount_paid.return_value = 1.00
+
+    bake_sale = BakeSalePos(bake_sale_inventory, input_buffer, output_buffer)
+
+    # Act
+    bake_sale.process_order()
+
+    # Assert
+    output_buffer.write_message.assert_called_once_with("Not enough money")
+
+def test_multiple_items_in_order(bake_sale_inventory, input_buffer, output_buffer):
+    # Arrange
+    input_buffer.read_purchase_input.return_value = "B,M"
+    input_buffer.read_amount_paid.return_value = 2.00
+
+    bake_sale = BakeSalePos(bake_sale_inventory, input_buffer, output_buffer)
+
+    # Act
+    bake_sale.process_order()
+
+    # Assert
+    output_buffer.write_message.assert_called_once_with("Change due: $0.25")
 
